3360b5ca4f36f448b55cbda873e23744286d205a
Lucas Chang <lchang@LUCAS-CHANGS-LOANER.local>
2024-08-04 21:15:56 -0700

    no weather packing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template :class="$colorMode.value">
   <div
-    class="min-h-screen bg-sky-300 text-gray-950 transition duration-300 ease-in-out dark:bg-sky-900 dark:text-white"
+    class="min-h-screen bg-sky-200 text-gray-950 transition duration-300 ease-in-out dark:bg-sky-900 dark:text-white"
   >
     <slot />
     <ColorMode />
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="p-2 md:p-8">
     <div>
-      <p class="text-center text-7xl font-bold text-yellow-300">
+      <p class="text-center text-7xl font-bold">
         Packing List Generator
       </p>
       <div class="mx-auto flex w-5/6 flex-row space-x-4 pt-12">
-        <div class="flex w-4/6 flex-col">
+        <div class="flex w-3/6 flex-col">
           <UInput
             v-model="location"
             placeholder="Where are you going?"
@@ -13,20 +13,20 @@
           />
         </div>
         <div class="flex w-1/6 flex-col">
-          <UPopover :popper="{ placement: 'bottom-start' }">
-            <UButton
-              icon="i-heroicons-calendar-days-20-solid"
-              :label="format(date, 'd MMM, yyy')"
-            />
-
-            <template #panel="{ close }">
-              <DatePicker
-                v-model="date"
-                is-required
-                @close="close"
-              />
-            </template>
-          </UPopover>
+          <UInput
+            v-model="startDate"
+            type="date"
+            placeholder="Days"
+            icon="i-heroicons-calendar-days"
+          />
+        </div>
+        <div class="flex w-1/6 flex-col">
+          <UInput
+            v-model="days"
+            type="number"
+            placeholder="Days"
+            icon="i-heroicons-calendar-days"
+          />
         </div>
         <div class="flex w-1/6 flex-col">
           <UButton
@@ -38,9 +38,179 @@
           </UButton>
         </div>
       </div>
-      <pre v-if="weather">
-      {{ weather }}
-    </pre>
+      <div
+        v-if="location && startDate && days"
+        class="mt-12 p-4 text-center"
+      >
+        <p
+          class="text-3xl font-bold"
+        >
+          Packing list for {{ location }} from {{ useDateFormat(startDate, 'ddd MMMM D YYYY').value }} to {{ useDateFormat(endDate, 'ddd MMMM D YYYY').value }}
+        </p>
+        <div class="mt-8 flex flex-row">
+          <div class="flex w-1/5 flex-col">
+            <p class="text-2xl font-semibold">
+              Clothes
+            </p>
+            <div class="mx-auto mt-2">
+              <UCheckbox
+                :label="`${days.toString()} Shirt${days > 1 ? 's': ''}`"
+                required
+              />
+              <UCheckbox
+                :label="`${days.toString()} Short${days > 1 ? 's': ''}`"
+                required
+              />
+              <UCheckbox
+                :label="`${days.toString()} Underpants`"
+                required
+              />
+              <UCheckbox
+                :label="`${days.toString()} Socks`"
+                required
+              />
+              <UCheckbox
+                label="1 Hoodie"
+                required
+              />
+            </div>
+          </div>
+          <div class="flex w-1/5 flex-col">
+            <p class="text-2xl font-semibold">
+              Running Clothes
+            </p>
+            <div class="mx-auto mt-2">
+              <UCheckbox
+                :label="`${days.toString()} Shirt${days > 1 ? 's': ''}`"
+                required
+              />
+              <UCheckbox
+                :label="`${days.toString()} Short${days > 1 ? 's': ''}`"
+                required
+              />
+              <UCheckbox
+                :label="`${days.toString()} Socks`"
+                required
+              />
+              <UCheckbox
+                label="Trainers"
+                required
+              />
+              <UCheckbox label="Foam Roller" />
+              <UCheckbox
+                label="Stretching Rope"
+                required
+              />
+              <UCheckbox label="Exercise Band" />
+              <UCheckbox label="Normatec" />
+              <UCheckbox label="Trainers 2" />
+              <UCheckbox label="Spikes" />
+              <UCheckbox label="Vaporflies" />
+            </div>
+          </div>
+          <div class="flex w-1/5 flex-col">
+            <p class="text-2xl font-semibold">
+              Toiletries
+            </p>
+            <div class="mx-auto mt-2">
+              <UCheckbox
+                label="Zyrtec"
+                required
+              />
+              <UCheckbox
+                label="Iron"
+                required
+              />
+              <UCheckbox
+                label="CeraVe"
+                required
+              />
+              <UCheckbox
+                label="Lip Balm"
+                required
+              />
+              <UCheckbox
+                label="Special Lotion"
+                required
+              />
+              <UCheckbox
+                label="Floss"
+                required
+              />
+              <UCheckbox
+                label="Toothpaste"
+                required
+              />
+              <UCheckbox
+                label="Toothbrush"
+                required
+              />
+              <UCheckbox
+                label="Suncreen"
+                required
+              />
+              <UCheckbox
+                label="Deodorant"
+                required
+              />
+              <UCheckbox
+                label="Melatonin"
+              />
+              <UCheckbox
+                label="Flonase"
+              />
+              <UCheckbox
+                v-if="days > 2"
+                label="Loofah"
+                required
+              />
+            </div>
+          </div>
+          <div class="flex w-1/5 flex-col">
+            <p class="text-2xl font-semibold">
+              Entertainment
+            </p>
+            <div class="mx-auto mt-2">
+              <UCheckbox
+                label="Headphones"
+              />
+              <UCheckbox
+                label="Book"
+              />
+              <UCheckbox
+                label="Stuffy"
+              />
+            </div>
+          </div>
+          <div class="flex w-1/5 flex-col">
+            <p class="text-2xl font-semibold">
+              Other
+            </p>
+            <div class="mx-auto mt-2">
+              <UCheckbox
+                label="Eye Mask"
+              />
+              <UCheckbox
+                label="Journal"
+              />
+              <UCheckbox
+                label="Laundry Bag"
+              />
+              <UCheckbox
+                label="Clif Bar"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="mt-8">
+          <pre
+            v-if="weather"
+            class="text-left"
+          >
+        {{ weather }}
+      </pre>
+        </div>
+      </div>
     </div>
     <p class="pt-48 text-center">
       Powered by <a
@@ -52,11 +222,15 @@
 </template>
 
 <script setup lang="ts">
-import { format } from 'date-fns';
-
-const date = ref(new Date());
 const weather = ref(null);
 const location = ref('');
+const days = ref(1);
+const startDate = ref('');
+const endDate = computed(() => {
+  const d = new Date(startDate.value);
+  d.setDate(d.getDate() + days.value + 1);
+  return d;
+});
 
 async function getWeather(location: string) {
   try {
